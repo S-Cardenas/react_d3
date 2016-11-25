62de883bbf38f4918a1d747cb4a972194e0e4d5c
stefan <fets809@gmail.com>
2016-11-25 18:24:44 -0500

    updated margin of grouped_column chart

--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -5,7 +5,7 @@ import ReactDOM from 'react-dom';
 //Store and Root Component
 import configureStore from './store/store';
 import ColumnChart from './components/grouped_column/chart';
-import BarChart from './components/scatter_plot/chart';
+import BarChart from './components/grouped_column/chart';
 
 document.addEventListener("DOMContentLoaded", function() {
   ReactDOM.render(<BarChart />, document.getElementById('root'));
