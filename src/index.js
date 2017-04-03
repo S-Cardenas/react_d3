8dc4fdbd15db905eb03802195466b4b67b513356
stefan <fets809@gmail.com>
2017-04-02 21:43:55 -0400

    changed require path

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-exports.ColumnChart = require('../frontend/components/final_grouped_column/chart').default;
+exports.ColumnChart = require('../frontend/components/final_grouped_column/chart.js').default;
 exports.BarChart = require('../frontend/components/final_grouped_bar/chart').default;
 exports.LineChart = require('../frontend/components/final_line_chart/chart').default;
 exports.ScatterPlot = require('../frontend/components/final_scatter_plot/chart').default;
